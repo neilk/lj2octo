dee18531f0f49d08729c309512a8b546b6da2035
Neil Kandalgaonkar <neilk@brevity.org>
2013-04-23 01:47:25 -0700

    line oriented parsing of lj-cut, yaml matter

--- lj2octo.py
+++ lj2octo.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 from bs4 import BeautifulSoup, Comment, NavigableString
 import HTMLParser
+import re
 import sys
+import yaml
 
 htmlParser = HTMLParser.HTMLParser()
 
@@ -15,34 +17,21 @@ url = event.url.string
 
 bodyHtml = htmlParser.unescape(event.event.string)
 
-bodySoup = BeautifulSoup(bodyHtml)
+ljCutRe = r'<lj-cut [^>]+>'
+bodyHtml = re.sub(ljCutRe, '<!-- more -->\n', bodyHtml, 1)
+bodyHtml = re.sub(ljCutRe, '', bodyHtml)
 
-lj_cut = bodySoup.find_all('lj-cut')
-if len(lj_cut):
-    dummyMore = soup.new_tag('more')
-    lj_cut[0].insert_before(dummyMore)
-    for cut in lj_cut:
-        cut.unwrap()
-bodySoup.more.replace_with(Comment(" more "))
-
-doc = soup.new_tag('document');
-print "\n\n\n\n"
-for el in bodySoup.body.contents:
-    print "element!"
-    if isinstance(el, NavigableString) and not isinstance(el, Comment):
-        print "elll"
-        print el
-        lines = el.string.split(r'\r\n')
-        for line in lines:
-            print "line!" + line
-            para = soup.new_tag('p')
-            para.string = line
-            doc.append(para)
-    else:
-        doc.append(el)
-
-print eventTime
-print subject
-print doc.contents
-print url
+print "---"
+print yaml.dump({
+  'layout': 'post',
+  'title': str(subject),
+  'date': str(eventTime),
+  'published': 'true',
+  'categories': 'from-livejournal',
+  'dummy': []  # this is necessary for the yaml module to work :(
+})
+print "---"
+print bodyHtml
+print "<hr/>"
+print "originally from: " + url
 
